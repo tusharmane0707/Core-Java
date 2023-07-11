5212ec1ca9ef86d8d2bdc9a42319fe6378a40710
tusharmane0707 <88417632+tusharmane0707@users.noreply.github.com>
2023-07-11 10:04:42 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package june_23to30;
+
+public class Demo {
+	// class block
+	//compilation part
+
+	public static void main(String[] args) {
+		// main method block
+		// execution block start
+		System.out.println(" Hello World");
+
+	}
+
+}
